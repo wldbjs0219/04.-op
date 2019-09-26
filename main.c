ee300a6463628ff55296343d07c21d94d18a1434
wldbjs0219 <55428633+wldbjs0219@users.noreply.github.com>
2019-09-26 16:15:08 +0900

    04_03

--- main.c
+++ main.c
@@ -4,12 +4,18 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int x=3;
-	int cond1, cond2;
+    
+	int x, y;
+	  
+	printf("input two integers : ");
+	scanf(" %d %d", &x, &y);
 	
-	cond1=2<x && x<5;
-	cond2=x>1 || x<4 && x>3;
+	printf("+ result is %d\n", x+y);
+	printf("- result is %d\n", x-y);
+	printf("* result is %d\n", x*y);
+	printf("/ result is %d\n", x/y);
+	printf("%% result is %d\n", x%y);
 	
-	printf("cond1=%d,cond2=%d",
-	cond1,cond2);
+	return 0;
+	
 }
